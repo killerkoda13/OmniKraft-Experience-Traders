d7e2e7fa7f83e72cf258462fa2215bff5eec4bde
killerkoda13 <KillerKoda13>
2016-04-11 03:26:07 -0400

    Removed item id stuff. All trader creations will be done with item in hand because it's much easier. And ease is what it's alllll about baby.

--- me/killerkoda13/OmniExperienceTrader/OmniExperienceTrader.java
+++ me/killerkoda13/OmniExperienceTrader/OmniExperienceTrader.java
@@ -1,18 +1,27 @@
 package me.killerkoda13.OmniExperienceTrader;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class OmniExperienceTrader extends JavaPlugin{
 	
+	
+	static Plugin plugin;
+	
 	@Override
 	public void onEnable()
 	{
-
+		this.plugin = plugin;
 	}
 	
 	@Override
 	public void onDisable()
 	{
 		
+	}
+	
+	public static Plugin getInstance()
+	{
+		return plugin;
 	}
 }
--- me/killerkoda13/OmniExperienceTrader/Utils/Trader.java
+++ me/killerkoda13/OmniExperienceTrader/Utils/Trader.java
@@ -1,8 +1,14 @@
 package me.killerkoda13.OmniExperienceTrader.Utils;
 
+import me.killerkoda13.OmniExperienceTrader.OmniExperienceTrader;
+
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.plugin.Plugin;
 
 /***
  *		---------------------------------
@@ -14,107 +20,16 @@ import org.bukkit.inventory.ItemStack;
 public class Trader {	
 	
 	
+	Plugin plugin = OmniExperienceTrader.getInstance();			//Instance of plugin
+	
 	String line1;			//First line of trader
 	String line2;			//Second line of trader
 	int price;				//Price of item in trader
 	int amount;				//Amount of item trader is selling
-	int itemid;				//ID of item trader is selling
-	short data;				//Data of item being sold. Defaulted to 0 if none.
 	boolean gravity;		//If trader entity should have gravity
 	ItemStack hand;			//ItemStack object of item in players hand. Only used if item id is absent
 	World world;			//World the trader exists in
 	Location location;		//Location where the trader exists
-	
-	/**
-	 * @param firstline first line of trader
-	 * @param secondline second line of trader
-	 * @param price price of trader in experience
-	 * @param amount amount of item trader is selling
-	 * @param itemid id of item trader is selling
-	 * @param data data of item trader is selling
-	 * @param gravity if trader has gravity
-	 * @param world world the trader exists in
-	 * @param location location of trader
-	 */
-	public Trader(String firstline, String secondline, int price, int amount, int itemid, short data, boolean gravity, World world, Location location)
-	{
-		this.line1 = firstline;
-		this.line2 = secondline;
-		this.price = price;
-		this.amount = amount;
-		this.itemid = itemid;
-		this.data = data;
-		this.gravity = gravity;
-		this.world = world;
-		this.location = location;
-	}
-
-	/**
-	 * @param firstline first line of trader
-	 * @param secondline second line of trader
-	 * @param price price of trader in experience
-	 * @param amount amount of item trader is selling
-	 * @param itemid id of item trader is selling
-	 * @param gravity if trader has gravity
-	 * @param world world the trader exists in
-	 * @param location location of trader
-	 */
-	public Trader(String firstline, String secondline, int price, int amount, int itemid, boolean gravity, World world, Location location)
-	{
-		this.line1 = firstline;
-		this.line2 = secondline;
-		this.price = price;
-		this.amount = amount;
-		this.itemid = itemid;
-		this.gravity = gravity;
-		this.world = world;
-		this.location = location;
-	}
-	
-	/**
-	 * @param firstline first line of trader
-	 * @param secondline second line of trader
-	 * @param price price of trader in experience
-	 * @param amount amount of item trader is selling
-	 * @param itemid id of item trader is selling
-	 * @param data data of item trader is selling
-	 * @param world world the trader exists in
-	 * @param location location of trader
-	 */	
-	public Trader(String firstline, String secondline, int price, int amount, int itemid, short data, World world, Location location)
-	{
-		this.line1 = firstline;
-		this.line2 = secondline;
-		this.price = price;
-		this.amount = amount;
-		this.itemid = itemid;
-		this.data = data;
-		this.world = world;
-		this.location = location;
-
-	}
-	
-	/**
-	 * @param firstline first line of trader
-	 * @param secondline second line of trader
-	 * @param price price of trader in experience
-	 * @param amount amount of item trader is selling
-	 * @param itemid id of item trader is selling
-	 * @param data data of item trader is selling
-	 * @param world world the trader exists in
-	 * @param location location of trader
-	 */
-	public Trader(String firstline, String secondline, int price, int amount, int itemid, World world, Location location)
-	{
-		this.line1 = firstline;
-		this.line2 = secondline;
-		this.price = price;
-		this.amount = amount;
-		this.itemid = itemid;
-		this.world = world;
-		this.location = location;
-
-	}
 	
 	/**
 	 * @param firstline first line of trader
@@ -157,5 +72,25 @@ public class Trader {
 		this.gravity = gravity;
 		this.world = world;
 		this.location = location;
+	}
+
+	
+	/**
+	 * Creates trader
+	 * @return true if trader was created successfully. returns false if trader was not created successfully.
+	 */
+	public boolean createTrader()
+	{
+
+		
+		
+		ArmorStand hitbox = (ArmorStand) world.spawnEntity(location, EntityType.ARMOR_STAND);
+		hitbox.setMetadata("xptrader.line1", new FixedMetadataValue(plugin, line1));
+		hitbox.setMetadata("xptrader.line2", new FixedMetadataValue(plugin, line2));
+		hitbox.setMetadata("xptrader.price", new FixedMetadataValue(plugin, price));
+		hitbox.setMetadata("xptrader.data", new FixedMetadataValue(plugin, gravity));
+		hitbox.setMetadata("xptrader.data", new FixedMetadataValue(plugin, hand));
+
+
 	}
 }
