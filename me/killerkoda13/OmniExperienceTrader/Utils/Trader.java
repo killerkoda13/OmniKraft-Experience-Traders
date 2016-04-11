91a3d1b910c49065352d7a7e04b22bdd7f6b41db
killerkoda13 <KillerKoda13>
2016-04-11 01:19:52 -0400

    Trader object in progress; added case for each possibility.

---
+++ me/killerkoda13/OmniExperienceTrader/Utils/Trader.java
@@ -0,0 +1,33 @@
+package me.killerkoda13.OmniExperienceTrader.Utils;
+
+public class Trader {
+	
+	String line1;
+	String line2;
+	int price;
+	int amount;
+	int itemid;
+	short data;
+	boolean gravity;
+	
+	
+	public Trader(String firstline, String secondline, int price, int amount, int itemid, short data, boolean gravity)
+	{
+		
+	}
+
+	public Trader(String firstline, String secondline, int price, int amount, int itemid, boolean gravity)
+	{
+		
+	}
+	
+	public Trader(String firstline, String secondline, int price, int amount, int itemid, short data)
+	{
+		
+	}
+	
+	public Trader(String firstline, String secondline, int price, int amount, int itemid)
+	{
+		
+	}
+}
