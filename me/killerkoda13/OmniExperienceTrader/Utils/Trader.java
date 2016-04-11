e3c32caca0eb16dde7af8eac78371fd33833bbb1
killerkoda13 <KillerKoda13>
2016-04-11 01:24:24 -0400

    initialize all objects

--- me/killerkoda13/OmniExperienceTrader/Utils/Trader.java
+++ me/killerkoda13/OmniExperienceTrader/Utils/Trader.java
@@ -1,5 +1,7 @@
 package me.killerkoda13.OmniExperienceTrader.Utils;
 
+import org.bukkit.inventory.ItemStack;
+
 public class Trader {
 	
 	String line1;
@@ -9,25 +11,65 @@ public class Trader {
 	int itemid;
 	short data;
 	boolean gravity;
-	
+	ItemStack hand;
 	
 	public Trader(String firstline, String secondline, int price, int amount, int itemid, short data, boolean gravity)
 	{
-		
+		this.line1 = firstline;
+		this.line2 = secondline;
+		this.price = price;
+		this.amount = amount;
+		this.itemid = itemid;
+		this.data = data;
+		this.gravity = gravity;
 	}
 
 	public Trader(String firstline, String secondline, int price, int amount, int itemid, boolean gravity)
 	{
-		
+		this.line1 = firstline;
+		this.line2 = secondline;
+		this.price = price;
+		this.amount = amount;
+		this.itemid = itemid;
+		this.gravity = gravity;
 	}
 	
 	public Trader(String firstline, String secondline, int price, int amount, int itemid, short data)
 	{
-		
+		this.line1 = firstline;
+		this.line2 = secondline;
+		this.price = price;
+		this.amount = amount;
+		this.itemid = itemid;
+		this.data = data;
 	}
 	
 	public Trader(String firstline, String secondline, int price, int amount, int itemid)
 	{
-		
+		this.line1 = firstline;
+		this.line2 = secondline;
+		this.price = price;
+		this.amount = amount;
+		this.itemid = itemid;
+
+	}
+	
+	public Trader(String firstline, String secondline, int price, int amount, ItemStack hand)
+	{
+		this.line1 = firstline;
+		this.line2 = secondline;
+		this.price = price;
+		this.amount = amount;
+		this.hand = hand;
+	}
+	
+	public Trader(String firstline, String secondline, int price, int amount, ItemStack hand, boolean gravity)
+	{
+		this.line1 = firstline;
+		this.line2 = secondline;
+		this.price = price;
+		this.amount = amount;
+		this.hand = hand;
+		this.gravity = gravity;
 	}
 }
